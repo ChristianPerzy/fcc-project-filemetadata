0b1791ccdc0caa135e8eeefa5f3f08fc0be2ecb5
ChristianPerzy <christian.perzy@gmail.com>
2022-06-10 05:19:22 +0200

    implemented POST /api/fileanalyse

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
+let multer = require('multer');
+let fs = require('node:fs');
 require('dotenv').config()
 
 var app = express();
+let fupload = multer({dest: 'uploads/'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +14,18 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', fupload.single('upfile'), (req, res) => {
+  let file = req.file;
+  fs.unlink(file.path, (err) => {
+    if (err) return console.log(err);
+  });  
 
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
